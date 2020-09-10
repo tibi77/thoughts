46b79a302ba8e955c0ff406636f534438f954d7c
tibi77 <tiberiulepadatu14@gmail.com>
2020-09-10 09:51:39 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-udo ffmpeg -f x11grab -r 60 -s 1920x1080 -i some_file.mp4 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 -vf 'hflip,scale=640:360' /dev/video0
+sudo ffmpeg -f x11grab -r 60 -s 1920x1080 -i some_file.mp4 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 -vf 'hflip,scale=640:360' /dev/video0
